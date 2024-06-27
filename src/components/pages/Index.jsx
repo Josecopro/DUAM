c6a652d58cecec7d5dd83309fecb8c5bc97609ad
Josecopro <juanbece2006@gmail.com>
2024-06-27 11:51:42 -0500

    refactor: maaking better cards and changing the info

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,79 +1,62 @@
 import "./styles/index.scss";
+
 import { AreaCard } from "../molecules/Card";
-import { useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
 
 function Index() {
-useEffect(() => {
 
-
-}, []);
-
-const navigate = useNavigate();
-
-const goToRoute = (route) => {
-  navigate(route);
-};
   return (
     <div>
       <h1>DUAM</h1>
         <p>Welcome to the home page!</p>
         <section className="AreasGrid">
         <AreaCard
-            name="Matemáticas y ciencias naturales"
+            name="Matem&aacute;ticas y ciencias naturales"
             image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-            description=" Las matem&aacute;ticas y las ciencias naturales son un equipo inseparable: un lenguaje universal para descifrar y un grupo de exploradores para descubrir los secretos del universo. ¡Juntas nos permiten armar el rompecabezas y entender la maravillosa complejidad del mundo que nos rodea!."
+            description="Explora la belleza de los patrones, descubre las leyes que rigen el cosmos y entiende la danza de la vida."
             link="/Matematicasyciencias"/>
 
         <AreaCard
-            name="Ciencias sociales y humanidades"
+            name="Ciencias de la educaci&oacute;n"
             image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
-            description="Las ciencias sociales y las humanidades son como un espejo que nos permite ver y comprender la historia, la cultura y la sociedad en la que vivimos. Nos ayudan a entender quiénes somos, de dónde venimos y hacia dónde vamos."
+            description="Comprende los procesos del aprendizaje, guía a las mentes jóvenes y transforma vidas."
             link="/Matematicasyciencias"/>
         
         <AreaCard
-            name="Artes y letras"
+            name="Ingenier&iacute;a, arquitectura y urbanismo"
             image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
-            description="Las artes y las letras son la forma en la que expresamos nuestra creatividad, emociones y pensamientos. Son una ventana al mundo y una puerta al corazón. ¡Nos permiten soñar, imaginar y crear un universo lleno de posibilidades infinitas!"
+            description="Diseña soluciones innovadoras, construye infraestructuras resilientes y modela espacios urbanos prósperos."
             link="https://www.pokemon.com/el/pokedex/squirtle"/>
         
         <AreaCard
-            name="Educación física y deportes"
+            name="Econom&iacute;a, administraci&oacute;n, contadur&iacute;a"
             image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-            description="La educación física y los deportes son una combinación perfecta para mantenernos activos, sanos y felices. Nos enseñan a trabajar en equipo, a superar desafíos y a disfrutar de la vida al máximo. ¡Juntos somos invencibles!"
+            description="Comprende las fuerzas económicas que impulsan el mercado, gestiona recursos de manera eficiente y toma decisiones estratégicas."
             link="https://www.pokemon.com/el/pokedex/bulbasaur"/>
           
         <AreaCard
-            name="Tecnología y programación"
+            name="Ciencias sociales y humanas"
             image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png"
-            description="La tecnología y la programación son el futuro: nos permiten crear, innovar y transformar el mundo que nos rodea. Son una herramienta poderosa para resolver problemas, comunicarnos y colaborar en equipo. ¡Juntos podemos cambiar el mundo!"
+            description="Explora las complejidades de la sociedad, desvela los misterios de la mente y descubre la riqueza de las culturas."
             link="https://www.pokemon.com/el/pokedex/caterpie"/>
 
         <AreaCard
-            name="Idiomas y comunicación"
+            name="Bellas artes"
             image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png"
-            description="Los idiomas y la comunicación son la clave para conectarnos con el mundo: nos permiten expresarnos, entendernos y colaborar con personas de todas partes. Son una puerta abierta a nuevas culturas, ideas y oportunidades. ¡Juntos somos más fuertes!"
+            description="Despierta tu creatividad, explora tu sensibilidad y plasma tu visión única del mundo."
             link="https://www.pokemon.com/el/pokedex/pidgey"/>
-                <AreaCard
-            name="Educación física y deportes"
-            image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-            description="La educación física y los deportes son una combinación perfecta para mantenernos activos, sanos y felices. Nos enseñan a trabajar en equipo, a superar desafíos y a disfrutar de la vida al máximo. ¡Juntos somos invencibles!"
-            link="https://www.pokemon.com/el/pokedex/bulbasaur"/>
-          
         <AreaCard
-            name="Tecnología y programación"
+            name="Agronom&iacute;a veterinaria y afines"
             image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png"
-            description="La tecnología y la programación son el futuro: nos permiten crear, innovar y transformar el mundo que nos rodea. Son una herramienta poderosa para resolver problemas, comunicarnos y colaborar en equipo. ¡Juntos podemos cambiar el mundo!"
+            description="Para la sociedad es esencial nutrir y proteger la vida en la Tierra."
             link="https://www.pokemon.com/el/pokedex/caterpie"/>
 
         <AreaCard
-            name="Idiomas y comunicación"
+            name="Ciencias de la salud"
             image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png"
-            description="Los idiomas y la comunicación son la clave para conectarnos con el mundo: nos permiten expresarnos, entendernos y colaborar con personas de todas partes. Son una puerta abierta a nuevas culturas, ideas y oportunidades. ¡Juntos somos más fuertes!"
+            description="Si te apasiona cuidar a los demás y contribuir al bienestar de la humanidad, las ciencias de la salud te ofrecen una carrera desafiante y gratificante."
             link="https://www.pokemon.com/el/pokedex/pidgey"/>
         </section>
 
-        <button onClick={goToRoute("/Matematicasyciencias")}></button>
     </div>
   );
 }
